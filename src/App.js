9897801a6e69003fdd220bf4f66027b7429c0a8a
juhi7782 <108575713+juhi7782@users.noreply.github.com>
2022-10-17 12:07:05 +0530

    chat bot

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import{ChatEngine} from 'react-chat-Engine';
+
+import './App.css';
+
+const App=()=>{
+return(
+
+<ChatEngine
+height="100vh"
+
+ProjectID="359c8da4-dc65-4d3e-bfa6-72bff4bade92"
+
+UserName="javascriptmaster"
+
+UserSecret="123123"
+/>
+ );
+}
+
+export default App;
